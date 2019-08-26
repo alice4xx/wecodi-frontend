7cd70daf9bd0cbf23a7f6416ff7e7418b964d10a
apollostk <apollostk20@gmail.com>
2019-08-26 21:08:28 +0900

    사인업 라우트 세팅

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './Pages/App';
 import Login from './Pages/Login/Login';
+import Signup from './Pages/Signup/Signup';
 
 class Routes extends Component {
   render() {
@@ -10,6 +11,7 @@ class Routes extends Component {
         <Switch>
           <Route exact path="/" component={App} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
         </Switch>
       </Router>
     );
