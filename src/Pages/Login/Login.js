e5048ecd4fa7b8143d84ed003c040c77ec0ec4e3
alice4xx <aliceaprilx@gmail.com>
2019-08-27 15:36:18 +0900

    wip

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,7 +3,26 @@ import './Login.scss';
 
 class Login extends Component {
   render() {
-    return <div className="login-page"> log in </div>;
+    return (
+      <div className="loginWrap">
+        <div className="login-page">
+          <div className="loginWording"> Log In </div>
+          <div className="emailpassword"> EMAIL</div>
+          <input
+            type="text"
+            className="addressBox"
+            placeholder="Email address"
+          />
+          <div className="emailpassword"> PASSWORD </div>
+          <input
+            type="password"
+            className="passwordBox"
+            placeholder="Password"
+          />
+          <button className="LogInBttn"> Log In </button>
+        </div>
+      </div>
+    );
   }
 }
 
