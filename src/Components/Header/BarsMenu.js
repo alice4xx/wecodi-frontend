79a0c400be1dd9489f3a70afe236def9c37cac1a
soongyu <soon9superman@gmail.com>
2019-08-30 20:40:20 +0900

    wip

--- src/Components/Header/BarsMenu.js
+++ src/Components/Header/BarsMenu.js
@@ -28,33 +28,17 @@ const BarsMenu = props => {
         </div>
       </div>
       <div className="ListWrap">
-        <div className="Category">
-          <div className="CategoryName">
-            <h3 data-path="home" onClick={goToPage}>
-              HOME
-            </h3>
-          </div>
+        <div className="Category" data-path="home" onClick={goToPage}>
+          <h3>HOME</h3>
         </div>
-        <div className="Category">
-          <div className="CategoryName">
-            <h3 data-path="fashion" onClick={goToPage}>
-              FASHION TIPS
-            </h3>
-          </div>
+        <div className="Category" data-path="fashion" onClick={goToPage}>
+          <h3>FASHION TIPS</h3>
         </div>
-        <div className="Category">
-          <div className="CategoryName" onClick={goToPage}>
-            <h3 data-path="outfit" onClick={goToPage}>
-              OUTFIT IDEAS
-            </h3>
-          </div>
+        <div className="Category" onClick={goToPage} data-path="outfit">
+          <h3>OUTFIT IDEAS</h3>
         </div>
-        <div className="Category">
-          <div className="CategoryName" onClick={goToPage}>
-            <h3 data-path="inside" onClick={goToPage}>
-              INSIDE STITCH FIX
-            </h3>
-          </div>
+        <div className="Category" data-path="inside" onClick={goToPage}>
+          <h3>INSIDE STITCH FIX</h3>
         </div>
       </div>
     </div>
