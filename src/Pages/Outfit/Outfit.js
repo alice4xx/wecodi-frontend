92104359a7feb2ca0437b363137e724ccca15949
soongyu <soon9superman@gmail.com>
2019-09-15 13:22:13 +0900

    outfit 페이지 태그 기능 구현

--- src/Pages/Outfit/Outfit.js
+++ src/Pages/Outfit/Outfit.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import './Outfit.scss';
 import Footer from '../../Components/Footer/Footer';
 import Readmore from '../../Components/Readmore/Readmore';
@@ -16,11 +16,41 @@ class Outfit extends Component {
       limit: 1,
       outfits: [],
       target: 0,
+      tagList: {
+        'Latest': 0,
+        'Summer-Outfits' : 1,
+        'Casual' : 2,
+        'Denim' : 3,
+        'Date' : 4,
+      }
     }
 
     this.token = localStorage.getItem('wecodi_token')
       ? localStorage.getItem('wecodi_token')
       : '';
+  }
+
+  getTagList = async (id) => {
+    const outfits = await this.getTagApi(id);
+    this.setState({
+      outfits
+    })
+  }
+
+  getTagApi = (id) => {
+    return fetch(
+      `http://13.125.254.18:8000/article/sort/3/${id}?offset=${
+        this.state.index
+      }&limit=${this.state.limit + this.state.index + 8}`,
+      {
+        method: 'GET',
+        headers: {
+          AUTHORIZATION: this.token,
+        }
+      }
+    )
+    .then(response => response.json())
+    .then(response => response.DATA)
   }
 
   getMore = () => {
@@ -123,13 +153,27 @@ class Outfit extends Component {
               </section>
             </section>
             <section className="FilteredGallery">
-              <ul className="TagsWrap">
-                <li className="TagsList">Latest</li>
-                <li className="TagsList">Summer-Outfits</li>
-                <li className="TagsList">Casual</li>
-                <li className="TagsList">Denim</li>
-                <li className="TagsList">Athleisure</li>
-                <li className="TagsList">Date</li>
+              <ul ref={this.ref} className="TagsWrap">
+                <li onClick={() => {
+                  this.setState({outfits: []})
+                  this.getOutfit();
+                }} className="TagsList">Latest</li>
+                <li onClick = {() => {
+                  this.setState({outfits: []})
+                  this.getTagList(1);
+                }} className="TagsList">Summer-Outfits</li>
+                <li onClick = {() => {
+                  this.setState({outfits: []})
+                  this.getTagList(2);
+                }} className="TagsList">Casual</li>
+                <li onClick = {() => {
+                  this.setState({outfits: []})
+                  this.getTagList(3);
+                }} className="TagsList">Denim</li>
+                <li onClick = {() => {
+                  this.setState({outfits: []})
+                  this.getTagList(4);
+                }} className="TagsList">Date</li>
               </ul>
               <div className="FilterdContainer">
                 {outfits.length > 0 ? (
